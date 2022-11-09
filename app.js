eb147d7c060833f112027513d0c41e9914f787ba
gracygupta <gracy2112004@akgec.ac.in>
2022-11-09 09:36:25 -0800

    Routes added

--- app.js
+++ app.js
@@ -20,6 +20,30 @@ app.get("/", async function (req, res) {
   return res.status(200).json({ message: "Logged in" });
 });
 
+//for getting register
+app.post("/signup", AuthController.register);
+
+//for login with authentification
+app.post("/login", AuthController.login);
+
+//password reset email sender
+app.post("/password/reset", auth, AuthController.resetEmail);
+
+//password reset
+app.post("/password/reser/:token", auth, AuthController.resetPassword);
+
+//fetches the nickname to logged user only
+app.get("user/nickname", auth, AuthController.get_nickname);
+
+//sets new nickname...accessible by logged in user only
+app.post("user/nickname", auth, AuthController.change_nickname);
+
+//deletes the user...accessible only to user
+app.get("admin/delete/:email");
+
+//makes any user admin...accessible only to admin
+app.get("admin/make_admin/:email");
+
 app.listen(port, function () {
   console.log(`server is up at ${port}`);
 });

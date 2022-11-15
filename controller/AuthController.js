48abcdfbab7727dc895b4c3c6773a85fb49aceba
gracygupta <gracy2112004@akgec.ac.in>
2022-11-15 11:02:06 -0800

    conditions added for api and interface usage

--- controller/AuthController.js
+++ controller/AuthController.js
@@ -3,8 +3,9 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const nodemailer = require("nodemailer");
 const { json } = require("body-parser");
-const SECRET_KEY = "AUTHORIZED";
+const SECRET_KEY = process.env.SECRET_KEY;
 const oneDay = 1000 * 60 * 60 * 24;
+const api = process.env.api;
 
 //Register controller and token generator
 const register = async (req, res) => {
@@ -12,14 +13,17 @@ const register = async (req, res) => {
     console.log(req.body);
     const existingUser = await User.findOne({ email: req.body.email });
     if (existingUser) {
-      res.render("error", {
-        message_1: "OOPS!",
-        message_2: "User already exist",
-        brace: "(",
-      });
-      // return res.status(400).json({
-      //   message: "User already exist",
-      // });
+      if (api) {
+        return res.status(400).json({
+          message: "User already exist",
+        });
+      } else {
+        res.render("error", {
+          message_1: "OOPS!",
+          message_2: "User already exist",
+          brace: "(",
+        });
+      }
     }
     if (req.body.password === req.body.cpassword) {
       const hashedPassword = await bcrypt.hash(req.body.password, 10);
@@ -30,40 +34,47 @@ const register = async (req, res) => {
         password: hashedPassword,
       });
       console.log("User Added");
-      req.message = "User registered";
-      res.redirect("/login?message=User Registered. Please login here!");
-      // res.render("login", { message: "User Registered. Please login here!" });
-      // res.status(201).json({
-      //   user: {
-      //     id: user._id,
-      //     email: user.email,
-      //     nickname: user.nickname,
-      //     role: user.role,
-      //   },
-      //   token: token,
-      //   message: "User registered successfully",
-      // });
+      if (api) {
+        res.status(201).json({
+          user: {
+            id: user._id,
+            email: user.email,
+            nickname: user.nickname,
+            role: user.role,
+          },
+          token: token,
+          message: "User registered successfully",
+        });
+      } else {
+        res.redirect("/login?message=User Registered. Please login here!");
+      }
     } else {
-      res.render("error", {
-        message_1: "SORRY!",
-        message_2: "Password do not match",
-        brace: "(",
-      });
-      // res.status(400).json({
-      //   message: "Password does not match",
-      // });
+      if (api) {
+        res.status(400).json({
+          message: "Password does not match",
+        });
+      } else {
+        res.render("error", {
+          message_1: "SORRY!",
+          message_2: "Password do not match",
+          brace: "(",
+        });
+      }
     }
   } catch (err) {
     console.log(err);
-    res.render("error", {
-      message_1: "OOPS!",
-      message_2: "Something went wrong",
-      brace: "(",
-    });
-    // res.status(500).json({
-    //   error: err,
-    //   message: "Something went wrong",
-    // });
+    if (api) {
+      res.status(500).json({
+        error: err,
+        message: "Something went wrong",
+      });
+    } else {
+      res.render("error", {
+        message_1: "OOPS!",
+        message_2: "Something went wrong",
+        brace: "(",
+      });
+    }
   }
 };
 
@@ -73,25 +84,31 @@ const login = async (req, res) => {
   try {
     const existingUser = await User.findOne({ email: email });
     if (!existingUser) {
-      res.render("error", {
-        message_1: "OOPS!",
-        message_2: "User not found",
-        brace: "(",
-      });
-      // return res.status(400).json({
-      //   message: "User not found",
-      // });
+      if (api) {
+        return res.status(400).json({
+          message: "User not found",
+        });
+      } else {
+        res.render("error", {
+          message_1: "OOPS!",
+          message_2: "User not found",
+          brace: "(",
+        });
+      }
     }
     const matchPassword = await bcrypt.compare(password, existingUser.password);
     if (!matchPassword) {
-      res.render("error", {
-        message_1: "SORRY!",
-        message_2: "Invalid Credentials",
-        brace: "(",
-      });
-      // return res.status(400).json({
-      //   message: "Invalid Credentials",
-      // });
+      if (api) {
+        return res.status(400).json({
+          message: "Invalid Credentials",
+        });
+      } else {
+        res.render("error", {
+          message_1: "SORRY!",
+          message_2: "Invalid Credentials",
+          brace: "(",
+        });
+      }
     }
     User.update({ email: existingUser }, { last_logged_in: Date.now() });
     const token = jwt.sign(
@@ -102,27 +119,32 @@ const login = async (req, res) => {
       expires: new Date(Date.now() + oneDay),
       httpOnly: true,
     });
-    res.redirect("/profile");
-
-    // res.status(201).json({
-    //   user: {
-    //     id: existingUser._id,
-    //     email: existingUser.email,
-    //     nickname: existingUser.nickname,
-    //   },
-    //   token: token,
-    // });
+    if (api) {
+      res.status(201).json({
+        user: {
+          id: existingUser._id,
+          email: existingUser.email,
+          nickname: existingUser.nickname,
+        },
+        token: token,
+      });
+    } else {
+      res.redirect("/profile");
+    }
   } catch (err) {
     console.log(err);
-    res.render("error", {
-      message_1: "OOPS!",
-      message_2: "Something went wrong",
-      brace: "(",
-    });
-    // res.status(500).json({
-    //   error: err,
-    //   message: "Something went wrong",
-    // });
+    if (api) {
+      res.status(500).json({
+        error: err,
+        message: "Something went wrong",
+      });
+    } else {
+      res.render("error", {
+        message_1: "OOPS!",
+        message_2: "Something went wrong",
+        brace: "(",
+      });
+    }
   }
 };
 
@@ -134,14 +156,17 @@ const resetEmail = async (req, res) => {
     let user = await User.findOne({ email: email });
     if (!user) {
       console.log("User not found");
-      res.render("error", {
-        message_1: "OOPS!",
-        message_2: "User not found",
-        brace: "(",
-      });
-      // return res.status(400).json({
-      //   message: "User not found",
-      // });
+      if (api) {
+        return res.status(400).json({
+          message: "User not found",
+        });
+      } else {
+        res.render("error", {
+          message_1: "OOPS!",
+          message_2: "User not found",
+          brace: "(",
+        });
+      }
     } else {
       //token creation
       const recovery_token = jwt.sign(
@@ -168,34 +193,44 @@ const resetEmail = async (req, res) => {
       transporter.sendMail(mailOptions, function (error, info) {
         if (error) {
           console.log(error);
-          res.render("error", {
-            message_1: "OOPS!",
-            message_2: "An error occured",
-            brace: "(",
-          });
-          // return res.status(500).json({
-          //   error: "An error occured",
-          // });
+
+          if (api) {
+            return res.status(500).json({
+              error: "An error occured",
+            });
+          } else {
+            res.render("error", {
+              message_1: "OOPS!",
+              message_2: "An error occured",
+              brace: "(",
+            });
+          }
         } else {
           console.log("Email sent" + info.response);
-          res.render("success", { message: "Email sent successfully" });
-          // return res.status(201).json({
-          //   status: "success",
-          //   message: "Email sent successfully",
-          // });
+          if (api) {
+            return res.status(201).json({
+              status: "success",
+              message: "Email sent successfully",
+            });
+          } else {
+            res.render("success", { message: "Email sent successfully" });
+          }
         }
       });
     }
   } catch (err) {
     console.log(err);
-    res.render("error", {
-      message_1: "OOPS!",
-      message_2: "Something went wrong",
-      brace: "(",
-    });
-    // res.status(500).json({
-    //   error: "Something went wrong",
-    // });
+    if (api) {
+      res.render("error", {
+        message_1: "OOPS!",
+        message_2: "Something went wrong",
+        brace: "(",
+      });
+    } else {
+      res.status(500).json({
+        error: "Something went wrong",
+      });
+    }
   }
 };
 
@@ -210,31 +245,42 @@ const resetPassword = async (req, res) => {
       );
       console.log(user);
       console.log("Password changed");
-      res.render("login", { message: "Password changed. You can login here!" });
-      // res.status(201).json({
-      //   message: "Password Changed",
-      // });
+      if (api) {
+        res.status(201).json({
+          message: "Password Changed",
+        });
+      } else {
+        res.render("login", {
+          message: "Password changed. You can login here!",
+        });
+      }
     } else {
       console.log("Password do not match");
-      res.render("error", {
-        message_1: "OOPS!",
-        message_2: "Password do not match",
-        brace: "(",
-      });
-      // res.status(400).json({
-      //   error: "Password do not match",
-      // });
+      if (api) {
+        res.status(400).json({
+          error: "Password do not match",
+        });
+      } else {
+        res.render("error", {
+          message_1: "OOPS!",
+          message_2: "Password do not match",
+          brace: "(",
+        });
+      }
     }
   } catch (err) {
     console.log(err);
-    res.render("error", {
-      message_1: "OOPS!",
-      message_2: "Something went wrong",
-      brace: "(",
-    });
-    // res.status(500).json({
-    //   error: "Something went wrong",
-    // });
+    if (api) {
+      res.status(500).json({
+        error: "Something went wrong",
+      });
+    } else {
+      res.render("error", {
+        message_1: "OOPS!",
+        message_2: "Something went wrong",
+        brace: "(",
+      });
+    }
   }
 };
 
@@ -244,35 +290,45 @@ const get_nickname = async (req, res) => {
     const user = await User.findOne({ _id: req.userId });
     if (user) {
       console.log("Nickname:", user.nickname);
-      res.render("profile", {
-        name: user.nickname,
-        email: user.email,
-        role: user.role,
-      });
-      // res.status(201).json({
-      //   message: `Your nickname is ${user.nickname}`,
-      // });
+
+      if (api) {
+        res.status(201).json({
+          message: `Your nickname is ${user.nickname}`,
+        });
+      } else {
+        res.render("profile", {
+          name: user.nickname,
+          email: user.email,
+          role: user.role,
+        });
+      }
     } else {
       console.log("Error finding user");
-      res.render("error", {
-        message_1: "SORRY",
-        message_2: "Error finding user",
-        brace: "(",
-      });
-      // res.status(400).json({
-      //   message: "Error finding user",
-      // });
+      if (api) {
+        res.status(400).json({
+          message: "Error finding user",
+        });
+      } else {
+        res.render("error", {
+          message_1: "SORRY",
+          message_2: "Error finding user",
+          brace: "(",
+        });
+      }
     }
   } catch (err) {
     console.log(err);
-    res.render("error", {
-      message_1: "OOPS!",
-      message_2: "Something went wrong",
-      brace: "(",
-    });
-    // res.status(500).json({
-    //   message: "Something went wrong",
-    // });
+    if (api) {
+      res.status(500).json({
+        message: "Something went wrong",
+      });
+    } else {
+      res.render("error", {
+        message_1: "OOPS!",
+        message_2: "Something went wrong",
+        brace: "(",
+      });
+    }
   }
 };
 
@@ -282,10 +338,13 @@ const change_nickname = async (req, res) => {
   console.log(
     await User.updateOne({ email: req.userEmail }, { nickname: new_nickname })
   );
-  res.redirect("/user/nickname");
-  // res.status(201).json({
-  //   message: `Nickname updated to ${new_nickname}`,
-  // });
+  if (api) {
+    res.status(201).json({
+      message: `Nickname updated to ${new_nickname}`,
+    });
+  } else {
+    res.redirect("/user/nickname");
+  }
 };
 
 //delete user->done by only admin
@@ -297,34 +356,46 @@ const delete_user = async (req, res) => {
       if (admin.role === "admin") {
         if (admin.email === req.params.email) {
           await User.deleteOne({ email: req.params.email });
-          res.redirect("/login?message=Account Deleted");
+          if (api) {
+            return res.status(201).json({ message: "User Deleted" });
+          } else {
+            res.redirect("/login?message=Account Deleted");
+          }
         } else {
           console.log(
             await User.deleteOne({ email: req.params.email }),
             "\n user deleted"
           );
-          res.render("success", { message: "User Deleted" });
-
-          // res.status(201).json({ message: "User Deleted" });
+          if (api) {
+            res.status(201).json({ message: "User Deleted" });
+          } else {
+            res.render("success", { message: "User Deleted" });
+          }
         }
       } else {
         console.log("Unauthorized to delete");
-        res.render("error", {
-          message_1: "SORRY!",
-          message_2: "Not Authorized to do that",
-          brace: "(",
-        });
-        // res.status(400).json({ error: "Not Authorized to do that" });
+        if (api) {
+          res.status(400).json({ error: "Not Authorized to do that" });
+        } else {
+          res.render("error", {
+            message_1: "SORRY!",
+            message_2: "Not Authorized to do that",
+            brace: "(",
+          });
+        }
       }
     }
   } catch (err) {
     console.log(err);
-    res.render("error", {
-      message_1: "OOPS!",
-      message_2: "Something went wrong",
-      brace: "(",
-    });
-    // res.status(500).json({ error: "Something went wrong!!" });
+    if (api) {
+      res.status(500).json({ error: "Something went wrong!!" });
+    } else {
+      res.render("error", {
+        message_1: "OOPS!",
+        message_2: "Something went wrong",
+        brace: "(",
+      });
+    }
   }
 };
 
@@ -339,26 +410,38 @@ const change_role = async (req, res) => {
         await User.updateOne({ email: user_email }, { role: "admin" }),
         "\nUser is authorized as an Admin"
       );
-      res.render("success", { message: "User is authorized for admin's role" });
-
-      // res.status(201).json({ message: "User is authorized for admin's role" });
+      if (api) {
+        res
+          .status(201)
+          .json({ message: "User is authorized for admin's role" });
+      } else {
+        res.render("success", {
+          message: "User is authorized for admin's role",
+        });
+      }
     } else {
       console.log("User not found");
-      res.render("error", {
-        message_1: "OOPS!",
-        message_2: "User not found",
-        brace: "(",
-      });
-      // res.status(400).json({ error: "User not found" });
+      if (api) {
+        res.status(400).json({ error: "User not found" });
+      } else {
+        res.render("error", {
+          message_1: "OOPS!",
+          message_2: "User not found",
+          brace: "(",
+        });
+      }
     }
   } else {
     console.log("Unauthorized");
-    res.render("error", {
-      message_1: "SORRY!",
-      message_2: "Not Authorized to do that",
-      brace: "(",
-    });
-    // res.status(400).json({ message: "Not Authorized to do that" });
+    if (api) {
+      res.status(400).json({ message: "Not Authorized to do that" });
+    } else {
+      res.render("error", {
+        message_1: "SORRY!",
+        message_2: "Not Authorized to do that",
+        brace: "(",
+      });
+    }
   }
 };
 
